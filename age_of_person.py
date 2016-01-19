bd6742687c76c02558103b4353facc8d6a7fc7d7
pp <pp@users.noreply.github.com>
2016-01-19 08:25:55 +0100

    Extend functionality

--- age_of_person.py
+++ age_of_person.py
@@ -1,6 +1,7 @@
 """
-Takes the full name of a person as input and returns the
-person's current age and date of birth.
+Takes the full name of a person as input and returns the person's date
+of birth and current age. If the person is deceased, the date of birth
+and death and the age at death is returned.
 """
 
 import datetime
@@ -18,15 +19,28 @@ r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data, 'html.parser')
 
-age = soup.find('span', {'class':'bday'})
-age = ''.join(map(str, age.contents)).strip()
-age = datetime.datetime.strptime(age, '%Y-%m-%d')
-agef = age.strftime('%B %d, %Y')
+bday = soup.find('span', {'class':'bday'})
+if bday is not None:
+    bday = ''.join(map(str, bday.contents)).strip()
+    bday = datetime.datetime.strptime(bday, '%Y-%m-%d')
 
-now = datetime.datetime.now()
-nowf = now.strftime('%Y-%m-%d')
+dday = soup.find('span', {'class':'dday'})
+if dday is not None:
+    dday = ''.join(map(str, dday.contents)).strip()
+    dday = datetime.datetime.strptime(dday, '%Y-%m-%d')
 
-diff = relativedelta(now, age).years
-
-print(str(diff) + ' years')
-print(agef)
+if bday is not None:
+    born = 'Born: ' + bday.strftime('%B %d, %Y')
+    if dday is None:
+        now = datetime.datetime.now()
+        diff = relativedelta(now, bday).years
+        born += ' (age {0})'.format(diff)
+        print(born)
+    else:
+        diff = relativedelta(dday, bday).years
+        died = ('Died: ' + dday.strftime('%B %d, %Y') +
+               ' (aged {0})'.format(diff))
+        print(born)
+        print(died)
+else:
+    print('There is no person with this name.')
